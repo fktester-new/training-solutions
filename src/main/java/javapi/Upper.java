3878a9544b1f319a69da708a124cafab8947c5ff
fktester <kofe16@gmail.com>
2020-11-06 21:48:21 +0100

    Upper

---
+++ src/main/java/javapi/Upper.java
@@ -0,0 +1,8 @@
+package javapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String text = "Hello World!";
+        System.out.println(text.toUpperCase());
+    }
+}
