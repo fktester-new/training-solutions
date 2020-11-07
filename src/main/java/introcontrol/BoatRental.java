330cf41c45837bea06e8a601d136c8dc2930d5c0
fktester <kofe16@gmail.com>
2020-11-07 22:21:55 +0100

    BoatRental

---
+++ src/main/java/introcontrol/BoatRental.java
@@ -0,0 +1,47 @@
+package introcontrol;
+
+import java.util.Scanner;
+
+public class BoatRental {
+    public static void main(String[] args) {
+        int boat1Capacity = 5;
+        int boat2Capacity = 3;
+        int boat3Capacity = 2;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hány fős társaság érkezett?");
+
+        int numberOfPeople = scanner.nextInt();
+
+        int freePlaces = 10;
+        int freeBoats = 3;
+
+        if (numberOfPeople > 3) {
+            System.out.println("Ötös csónak");
+            numberOfPeople = numberOfPeople - 5;
+            freeBoats --;
+            freePlaces = freePlaces - 5;
+        }
+
+        if (numberOfPeople > 2) {
+            System.out.println("Hármas csónak");
+            numberOfPeople = numberOfPeople - 3;
+            freeBoats --;
+            freePlaces = freePlaces - 3;
+        }
+
+        if (numberOfPeople > 0) {
+            System.out.println("Kettes csónak");
+            numberOfPeople = numberOfPeople - 2;
+            freeBoats --;
+            freePlaces = freePlaces - 2;
+        }
+
+        if (numberOfPeople > 0) {
+            System.out.println("Sajnos nem fért el mindenki a csónakokban.");
+        } else {
+            System.out.println("A szabad helyek száma: " + freePlaces);
+            System.out.println("Az üres csónakok száma: " + freeBoats);
+        }
+    }
+}
