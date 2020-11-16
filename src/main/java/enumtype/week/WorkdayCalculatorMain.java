e4f4ea698a5281e0112fa7b234e0ff1cd208d6bb
fktester <kofe16@gmail.com>
2020-11-16 21:35:10 +0100

    Week

---
+++ src/main/java/enumtype/week/WorkdayCalculatorMain.java
@@ -0,0 +1,10 @@
+package enumtype.week;
+
+public class WorkdayCalculatorMain {
+
+    public static void main(String[] args) {
+        System.out.println(new WorkdayCalculator().dayTypes(Day.TUESDAY, 6));
+    }
+}
+
+
