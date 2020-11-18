ffa1bdfe260b3b7a9755e038454a8628f0ee023c
fktester <kofe16@gmail.com>
2020-11-18 20:14:30 +0100

    MathOperations

---
+++ src/main/java/numbers/MathOperations.java
@@ -0,0 +1,25 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class MathOperations {
+    public static void main(String[] args) {
+
+        String problem = "8 / (8 - 2) + 5 / (8 - 5)";
+        double result = 8.0 / (8 - 2) + 5.0 / (8 - 5);
+        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Mi a következő művelet eredménye? Négy tizedesjegy pontossággal kérem.");
+        System.out.println(problem);
+
+        double resultByUser = scanner.nextDouble();
+
+        if (Math.abs(result-resultByUser) < 0.0001) {
+            System.out.println("Helyes válasz!");
+        } else {
+            System.out.println("Hibás válasz!");
+        }
+
+    }
+}
