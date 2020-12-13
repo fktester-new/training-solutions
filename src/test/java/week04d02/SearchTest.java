5cf8ba5547b6ad937637290e6ee944aee33aa8e0
fktester <kofe16@gmail.com>
2020-12-13 22:02:27 +0100

    Search

--- src/test/java/week04d02/SearchTest.java
+++ src/test/java/week04d02/SearchTest.java
@@ -1,10 +1,10 @@
-package week04;
+package week04d02;
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SearchTest {
 
@@ -15,16 +15,10 @@ public class SearchTest {
     }
     @Test
     public void Given_a_string_and_a_char_When_there_is_one_match_Then_the_proper_index_is_returned() {
-        Assertions.assertEquals(
-                Arrays.asList(1),
-                target.getIndexesOfChar("ketchup", 'e')
-        );
+        assertEquals(Arrays.asList(1), target.getIndexesOfChar("ketchup", 'e'));
     }
     @Test
     public void Given_a_string_and_a_char_When_there_is_more_than_one_match_Then_the_proper_indexes_are_returned() {
-        Assertions.assertEquals(
-                Arrays.asList(1, 3),
-                target.getIndexesOfChar("kefe", 'e')
-        );
+        assertEquals(Arrays.asList(1, 3), target.getIndexesOfChar("kefe", 'e'));
     }
 }
