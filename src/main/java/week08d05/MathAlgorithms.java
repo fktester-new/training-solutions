21380231e816c7eb21f2c9f93520a4aa8d501d58
fktester <kofe16@gmail.com>
2020-12-19 16:45:50 +0100

    MathAlgorithms_v1

--- src/main/java/week08d05/MathAlgorithms.java
+++ src/main/java/week08d05/MathAlgorithms.java
@@ -3,13 +3,13 @@ package week08d05;
 public class MathAlgorithms {
 
     public int greatestCommonDivisor(int a, int b){
-        boolean x = a <= 0 || !(a == (int) a);
-        boolean y = b <= 0 || !(b == (int) b);
+        boolean x = ((a <= 0) || (b <= 0));
+        boolean y = (!(a == (int) a)|| !(b == (int) b));
         if(x || y){
             throw new IllegalArgumentException("I need positive integers!");
         }
 
-        int i = Math.min(a, b) / 2;
+        int i = Math.min(a, b);
         int d = 1;
         for (int j = 2; j <= i; j++){
             if (a % j == 0 && b % j == 0){
--- src/test/java/week08d05/MathAlgorithmsTest.java
+++ src/test/java/week08d05/MathAlgorithmsTest.java
@@ -2,15 +2,28 @@ package week08d05;
 
 import org.junit.jupiter.api.Test;
 
+import java.beans.Expression;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MathAlgorithmsTest {
 
     @Test
     public void greatestCommonDivisorTest(){
         MathAlgorithms algorithms = new MathAlgorithms();
-        assertEquals(12, algorithms.greatestCommonDivisor(48, 60));
+        assertEquals(30, algorithms.greatestCommonDivisor(30, 60));
         assertEquals(7, algorithms.greatestCommonDivisor(63, 35));
+        assertEquals(1, algorithms.greatestCommonDivisor(15, 28));
+
+    }
+
+    @Test
+    public void invalidInputTest(){
+        MathAlgorithms algorithms2 = new MathAlgorithms();
+        assertThrows(IllegalArgumentException.class, () -> algorithms2.greatestCommonDivisor(24, -343));
+        // assertThrows(IllegalArgumentException.class, () -> algorithms2.greatestCommonDivisor(12, 24.5));
+
 
     }
 }
