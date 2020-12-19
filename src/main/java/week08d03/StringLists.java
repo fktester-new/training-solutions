bec192827e7e0fc6f057c63e98a177d9cb330c62
fktester <kofe16@gmail.com>
2020-12-19 19:11:22 +0100

    StringLists_v1

--- src/main/java/week08d03/StringLists.java
+++ src/main/java/week08d03/StringLists.java
@@ -1,14 +1,37 @@
 package week08d03;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class StringLists {
 
     public List<String> stringListsUnion(List<String> first, List<String> second){
-        List<String> result = first;
+        List<String> result = new ArrayList<>();
+                result.addAll(first);
         for (String string: second) {
             if (!result.contains(string)){
                 result.add(string);
+            }
+        }
+        return result;
+    }
+
+    private boolean containsIgnoreCase(List<String> words, String element){
+        for (String s: words) {
+            if (s.toUpperCase().equals(element.toUpperCase())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<String> stringListsUnionIgnoreCase(List<String> first, List<String> second) {
+        List<String> result = new ArrayList<>();
+        result.addAll(first);
+        for (String str: second) {
+            if (!containsIgnoreCase(result, str)){
+                result.add(str);
             }
         }
         return result;
--- src/test/java/week08d03/StringListsTest.java
+++ src/test/java/week08d03/StringListsTest.java
@@ -19,11 +19,32 @@ public class StringListsTest {
         first.add("bb");
         first.add("cc");
         first.add("dd");
+
         List<String> second = Arrays.asList("bb", "dd", "ee", "ff");
-        List<String> result = first;
+
+        List<String> result = new ArrayList<>();
+        result.addAll(first);
         result.add("ee");
         result.add("ff");
 
         assertEquals(result, stringLists.stringListsUnion(first, second));
+    }
+
+    @Test
+    public void stringListsUnionIgnoreCaseTest(){
+        StringLists stringLists = new StringLists();
+        List<String> first = new ArrayList<>();
+        first.add("alma");
+        first.add("dió");
+        first.add("körte");
+        first.add("Banán");
+
+        List<String> second = Arrays.asList("banán", "szilva", "Alma");
+        List<String> result = new ArrayList<>();
+        result.addAll(first);
+        result.add("szilva");
+
+        assertEquals(result, stringLists.stringListsUnionIgnoreCase(first, second));
+
     }
 }
