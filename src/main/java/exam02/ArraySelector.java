3b827bbc8e723462746de4fb84219c47aaab87d5
fktester <kofe16@gmail.com>
2021-01-06 18:12:40 +0100

    ArraySelector

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,4 +1,17 @@
 package exam02;
 
 public class ArraySelector {
+
+public String selectEvens(int[] numbers){
+    if(numbers.length == 0){
+        return "";
+    }
+    StringBuilder result = new StringBuilder("[");
+    result.append(numbers[0]);
+    for (int i = 2; i < numbers.length; i +=2){
+        result.append(", ").append(numbers[i]);
+    }
+    result.append("]");
+    return result.toString();
+}
 }
