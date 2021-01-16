3ba3c6d3d0bfb8ca257b8258c0ba3e0901b9c29d
fktester <kofe16@gmail.com>
2021-01-16 12:18:50 +0100

    Cv_edited

--- src/main/java/exam02/cv/Cv.java
+++ src/main/java/exam02/cv/Cv.java
@@ -1,12 +1,13 @@
 package exam02.cv;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cv {
 
 
     private String name;
-    private List<Skill> skills;
+    private List<Skill> skills = new ArrayList<>();
 
     public Cv(String name) {
         this.name = name;
@@ -20,31 +21,22 @@ public class Cv {
         return skills;
     }
 
-    public int findSkillLevelByName(String skillName){
-        if (!findSkillByName(skillName)){
-            throw new SkillNotFoundException("Skill not found!");
-        }
-        int result = 1;
-        for (Skill skill: skills) {
-            if (skillName.equals(skill.getName())){
-                result = skill.getLevel();
-            }
-        }
-        return result;
-    }
+    public int findSkillLevelByName(String skillName) {
 
-    public void addSkills(List<Skill> newSkills){
-        for (Skill skill : newSkills) {
-            skills.add(skill);
-        }
-    }
-
-    private boolean findSkillByName(String skillName){
         for (Skill skill : skills) {
-            if (skillName.equals(skill.getName())){
-                return true;
+            if (skillName.equals(skill.getName())) {
+                return skill.getLevel();
             }
-            }
-        return false;
+        }
+        throw new IllegalArgumentException("Skill not found.");
     }
+
+    public void addSkills(String... newSkills) {
+        for (String s : newSkills) {
+            String name = s.substring(0, s.indexOf("(")).trim();
+            int level = Integer.parseInt(s.substring(s.indexOf("(") + 1, s.indexOf(")")));
+            skills.add(new Skill(name, level));
+        }
+    }
+
 }
