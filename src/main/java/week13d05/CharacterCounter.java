53d987dd575c4e96d4c7d4c884be45199288e517
fktester <kofe16@gmail.com>
2021-01-29 22:07:51 +0100

    CharacterCounter

---
+++ src/main/java/week13d05/CharacterCounter.java
@@ -0,0 +1,51 @@
+package week13d05;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class CharacterCounter {
+
+    public int countNnumberOfDifferentCharacters(String text){
+
+        if (text == null || text.isBlank()){
+            throw new IllegalArgumentException("The string is empty or null");
+        }
+        char[] characters  = text.toLowerCase().toCharArray();
+        Set<Character> result = new HashSet<>();
+
+        for (Character c : characters){
+            if (isValidCharacter(c)){
+                result.add(c);
+            }
+        }
+        return result.size();
+    }
+
+    public int countCharactersInMap(String word){
+        if (word == null || word.isBlank()){
+            throw new IllegalArgumentException("The string is empty or null");
+        }
+        char[] characters  = word.toLowerCase().toCharArray();
+        Map<Character, Integer> result = new HashMap<>();
+        for (Character c : characters) {
+            if (isValidCharacter(c)){
+                addToMap(result, c);
+            }
+        }
+        return result.size();
+    }
+
+    private boolean isValidCharacter(char c){
+        return ('a' <= c && c <= 'z');
+    }
+
+    private void addToMap(Map<Character, Integer> map, Character c){
+        if (map.containsKey(c)){
+            map.put(c, map.get(c) + 1);
+        } else {
+            map.put(c, 1);
+        }
+    }
+}
---
+++ src/test/java/week13d05/CharacterCounterTest.java
@@ -0,0 +1,36 @@
+package week13d05;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterCounterTest {
+
+    CharacterCounter cc = new CharacterCounter();
+
+    @Test
+    public void testCountNnumberOfDifferentCharacters(){
+        int count = cc.countNnumberOfDifferentCharacters("almapaprika");
+        assertEquals(7, count);
+
+        String input = "The quick brown dog jumps over the lazy fox";
+        assertEquals(26, cc.countNnumberOfDifferentCharacters(input));
+    }
+
+    @Test
+    public void testCountCharactersInMap(){
+        assertEquals(7, cc.countCharactersInMap("almapaprika"));
+        String input2 = "The quick brown dog jumps over the lazy fox";
+        assertEquals(26, cc.countCharactersInMap(input2));
+    }
+
+    @Test
+    public void testInvalidInput(){
+        assertThrows(IllegalArgumentException.class, () -> cc.countNnumberOfDifferentCharacters(null));
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> cc.countNnumberOfDifferentCharacters("  \n"));
+        assertEquals("The string is empty or null", exception.getMessage());
+    }
+
+}
