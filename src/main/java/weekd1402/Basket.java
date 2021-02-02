388e3b0c025762c72f07f0944727837512f02e08
fktester <kofe16@gmail.com>
2021-02-02 17:11:25 +0100

    Basket

---
+++ src/main/java/weekd1402/Basket.java
@@ -0,0 +1,64 @@
+package weekd1402;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Basket {
+
+
+    private Map<String, List<String>> products = new HashMap<>();
+
+    public void readFileToMap(String fileName) {
+        Path file = Path.of(fileName);
+
+        try (BufferedReader br = Files.newBufferedReader(file)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String id = line.substring(0, line.indexOf(" "));
+                String b = line.substring(line.indexOf(" ") + 1);
+                List<String> basket = Arrays.asList(b.split(","));
+                products.put(id, basket);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't read file", e);
+        }
+
+    }
+
+    public List<String> listBasket(String id) {
+        List<String> result = new ArrayList<>();
+        for (String k : products.keySet()) {
+            if (id.equals(k)) {
+                result = products.get(k);
+            }
+        }
+        Collections.sort(result);
+        return result;
+    }
+
+
+    public int countProduct(String name) {
+        int counter = 0;
+        for (String currentKey : products.keySet()) {
+            if (products.get(currentKey).contains(name)) {
+                counter++;
+            }
+        }
+
+
+        return counter;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.readFileToMap("shopping_list.txt");
+        System.out.println(basket.countProduct("beer"));
+    }
+
+}
