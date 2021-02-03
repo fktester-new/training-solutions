d8756df0aae77b10f14f2b1e412c585c5793d705
fktester <kofe16@gmail.com>
2021-02-03 08:36:49 +0100

    Basket_withTest

--- src/main/java/week14d02/Basket.java
+++ src/main/java/week14d02/Basket.java
@@ -1,4 +1,4 @@
-package weekd1402;
+package week14d02;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -47,18 +47,43 @@ public class Basket {
                 counter++;
             }
         }
-
-
         return counter;
     }
 
+    public int getNumberOfGoods(String id) {
+        int counter = 0;
+        for (String k : products.keySet()) {
+            if (id.equals(k)) {
+                counter = products.get(k).size();
+            }
+        }
+        return counter;
+    }
 
+    public Map<String, Integer> stat() {
+        Set<String> items = new HashSet<>();
+        Map<String, Integer> statItem = new HashMap<>();
+        for (Map.Entry<String, List<String>> me : products.entrySet()) {
+            for(String st : me.getValue()) {
+                items.add(st);
+            }
+        }
+        for(String s: items) {
+            statItem.put(s, countProduct(s));
+        }
+        return statItem;
+    }
 
+    public Map<String, List<String>> getProducts() {
+        return products;
+    }
 
     public static void main(String[] args) {
         Basket basket = new Basket();
         basket.readFileToMap("shopping_list.txt");
         System.out.println(basket.countProduct("beer"));
+        System.out.println(basket.getNumberOfGoods("W34111"));
+        System.out.println(basket.stat().toString());
     }
 
 }
---
+++ src/test/java/week14d02/BasketTest.java
@@ -0,0 +1,47 @@
+package week14d02;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BasketTest {
+
+    Basket basket = new Basket();
+    Map<String, List<String>> products = new HashMap<>();
+
+
+    @Test
+    public void testReadFileToMap() {
+        basket.readFileToMap("shopping_list.txt");
+        assertEquals(9, basket.getProducts().keySet().size());
+    }
+
+    @Test
+    public void testListBasket(){
+        basket.readFileToMap("shopping_list.txt");
+        assertEquals("[basil, cheese, passata, pasta]",basket.listBasket("B341").toString());
+        }
+
+    @Test
+    public void testCountProduct(){
+        basket.readFileToMap("shopping_list.txt");
+        assertEquals(2, basket.countProduct("beer"));
+    }
+
+    @Test
+    public void testGetNumberOfGoods(){
+        basket.readFileToMap("shopping_list.txt");
+        assertEquals(8, basket.getNumberOfGoods("W34111"));
+    }
+
+    @Test
+    public void testStat(){
+        basket.readFileToMap("shopping_list.txt");
+        assertEquals(2, basket.stat().get("tomato"));
+    }
+}
