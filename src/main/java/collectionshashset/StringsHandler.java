4d4b792ef5aa46c24e1f22ef91ccb8a2df35518e
fktester <kofe16@gmail.com>
2021-02-14 22:14:06 +0100

    StringHandler

---
+++ src/main/java/collectionshashset/StringsHandler.java
@@ -0,0 +1,18 @@
+package collectionshashset;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringsHandler {
+
+    public Set<String> filterUniqueStrings(Collection<String> stringCollection){
+        Set<String> uniqueStrings = new HashSet<>(stringCollection);
+        return uniqueStrings;
+        }
+    public Set<String> selectIdenticalStrings(Set<String> setA, Set<String> setB){
+        Set<String> intersection = new HashSet<String>(setA);
+        intersection.retainAll(setB);
+        return intersection;
+    }
+}
