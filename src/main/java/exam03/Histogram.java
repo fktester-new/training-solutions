bce39164f2d919ad7385decfb4e2cb2299fa7fe9
fktester <kofe16@gmail.com>
2021-02-15 17:53:27 +0100

    Histogram

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,5 +1,28 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Histogram {
+
+    public String createHistogram(BufferedReader br){
+        StringBuilder result = new StringBuilder();
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                int i = Integer.parseInt(line.trim());
+                for (int j = 0; j < i; j++){
+                    result.append("*");
+                }
+                result.append("\n");
+            }
+        } catch (IOException ioe){
+            throw new IllegalStateException("Cannot read file!", ioe);
+        }
+        return result.toString();
+    }
+
+
 
 }
