20d5ef1058bcfa9aef61e9c03df2b39ced49732d
fktester <kofe16@gmail.com>
2021-04-10 20:59:47 +0200

    SalesAmountSumCalculator

---
+++ src/main/java/algorithmssum/sales/SalesAmountSumCalculator.java
@@ -0,0 +1,14 @@
+package algorithmssum.sales;
+
+import java.util.List;
+
+public class SalesAmountSumCalculator {
+
+    public int sumSalesAmount(List<Salesperson> salespersons){
+        int sum = 0;
+        for (Salesperson s : salespersons) {
+            sum += s.getAmount();
+        }
+        return sum;
+    }
+}
