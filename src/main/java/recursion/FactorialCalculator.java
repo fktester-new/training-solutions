093bb15d01e008cf4f5a1673379d1a659b1b80e4
fktester <kofe16@gmail.com>
2021-04-12 21:36:02 +0200

    FactorialCalculator

---
+++ src/main/java/recursion/FactorialCalculator.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class FactorialCalculator {
+
+    public long getFactorial(int number){
+        if (number > 1){
+            long solution = getFactorial(number - 1);
+            return number * solution;
+        } else {
+            return 1;
+        }
+    }
+}
