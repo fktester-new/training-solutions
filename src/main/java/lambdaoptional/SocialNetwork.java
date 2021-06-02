dc4bd834f05378319bb6ca51f9ec06a5936599ee
fktester <kofe16@gmail.com>
2021-06-02 21:43:43 +0200

    SocialNetwork

---
+++ src/main/java/lambdaoptional/SocialNetwork.java
@@ -0,0 +1,40 @@
+package lambdaoptional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class SocialNetwork {
+
+    private List<Member> members;
+
+    public SocialNetwork(List<Member> members) {
+        this.members = members;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public Optional<Member> findFirst(Predicate<Member> condition){
+        for (Member member : members) {
+            if (condition.test(member)){
+                return Optional.of(member);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Double> averageNumberOfSkills(){
+        if (members.size() == 0){
+            return Optional.empty();
+        }
+        int sum = 0;
+        for (Member member : members) {
+            sum += member.getSkills().size();
+        }
+        return Optional.of((double) sum / members.size());
+    }
+}
+
+
