de451570c504905df675cc72622072c68062fa26
fktester <kofe16@gmail.com>
2021-07-20 21:44:38 +0200

    Sorter

---
+++ src/main/java/jvm/SorterMain.java
@@ -0,0 +1,15 @@
+package jvm;
+
+import java.util.List;
+import java.util.Random;
+
+public class SorterMain {
+    public static void main(String[] args) {
+        while(true){
+            Random random = new Random();
+            int size = random.nextInt(500_000);
+            System.out.println("Creating and sorting a list with size " + size);
+            List<Integer> list = new Sorter().createListAndSort(size);
+        }
+    }
+}
